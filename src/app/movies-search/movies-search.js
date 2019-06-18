cb0b2bda9cd9e24598e3430a60715cc239b1d8ee
Sudhir Paturu <sudhir9p@gmail.com>
2019-06-18 18:53:08 +0530

    app changes

--- src/app/movies-search/movies-search.js
+++ src/app/movies-search/movies-search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Input } from '../common/input/input';
 import { Button } from '../common/button/button';
 import { MoviesList } from '../movies-list/movies-list';
+import './movies-search.css';
 
 export class MovieSearch extends React.Component {
     onSearchInputKeyUp = (e) => {
@@ -14,13 +15,13 @@ export class MovieSearch extends React.Component {
 
     render() {
         return (
-            <>
+            <div className="movie-search-contents">
                 <header>FIND Your Movie</header>
                 <Input placeholder="Browse movies" keyUpHandler={this.onSearchInputKeyUp}></Input>
                 <Button text="SEARCH" onclick={this.onSearchClick}></Button>
                 <MoviesList ></MoviesList>
 
-            </>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app.js';
 
-ReactDOM.render(<App title="SUDHIR"></App>, document.getElementById('root'));
+ReactDOM.render(<App title="Netflix Roulette"></App>, document.getElementById('root'));
